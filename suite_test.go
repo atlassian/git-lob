4236618151d1cbe9482c0623c37c0e0e94d70846
Steve Streeting <steve@stevestreeting.com>
2014-11-10 18:20:58 +0000

    Control output in test suite

--- suite_test.go
+++ suite_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"io/ioutil"
+	"log"
 	"os/exec"
 	"testing"
 )
@@ -10,6 +12,17 @@ import (
 func TestAll(t *testing.T) {
 	// Connect Ginkgo to Gomega
 	RegisterFailHandler(Fail)
+
+	// Set manual logging off
+	loggingOff := true
+	//loggingOff = false
+	if loggingOff {
+		errorFileLog = log.New(ioutil.Discard, "", 0)
+		errorConsoleLog = log.New(ioutil.Discard, "", 0)
+		debugLog = log.New(ioutil.Discard, "", 0)
+		outputLog = log.New(ioutil.Discard, "", 0)
+	}
+
 	// Run everything
 	RunSpecs(t, "Git Lob Test Suite")
 }
