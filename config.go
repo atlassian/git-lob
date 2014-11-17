317270e300e730af5b4291deba94fb02db709217
Steve Streeting <steve@stevestreeting.com>
2014-11-17 11:36:24 -0800

    Implemented ability to read gitconfig

--- config.go
+++ config.go
@@ -1,10 +1,21 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"os/user"
+	"path"
+	"regexp"
+	"strings"
+)
+
 // Options (command line or config file TODO)
 type Options struct {
-	// Output verbosely
+	// Output verbosely (to console & log)
 	Verbose bool
-	// Output quietly
+	// Output quietly (to console)
 	Quiet bool
 	// Never prompt for user input, rely on command line options only
 	NonInteractive bool
@@ -20,10 +31,162 @@ type Options struct {
 	EnableLogFile bool
 	// Log file (optional, defaults to ~/git-lob.log if not specified)
 	LogFile string
+	// Log verbosely even if main Verbose option is disabled for console
+	VerboseLog bool
+	// Combination of root .gitconfig and repository config as map
+	GitConfig map[string]string
 }
 
 func NewOptions() *Options {
 	return &Options{
 		StringOpts: make(map[string]string),
 		Args:       make([]string, 0, 5)}
+}
+
+// Read .gitconfig / .git/config for specific options to override
+// Returns a map of setting=value, where group levels are indicated by dot-notation
+// e.g. git-lob.logfile=blah
+// all keys are converted to lower case for easier matching
+func LoadConfig() map[string]string {
+	// Don't call out to 'git config' to read file, that's slower and forces a dependency on git
+	// which we may not want to have (e.g. support for libgit clients)
+	// Read files directly, it's a simple format anyway
+
+	// TODO system git config?
+
+	var ret map[string]string = nil
+
+	// User config
+	usr, err := user.Current()
+	if err != nil {
+		LogError("Unable to access user home directory")
+		// continue anyway
+	} else {
+		userConfigFile := path.Join(usr.HomeDir, ".gitconfig")
+		userConfig, err := ReadConfigFile(userConfigFile)
+		if err != nil {
+			if ret == nil {
+				ret = userConfig
+			} else {
+				for key, val := range userConfig {
+					ret[key] = val
+				}
+			}
+		}
+	}
+
+	// repo config
+	gitDir := GetGitDir()
+	repoConfigFile := path.Join(gitDir, "config")
+	repoConfig, err := ReadConfigFile(repoConfigFile)
+	if err != nil {
+		if ret == nil {
+			ret = repoConfig
+		} else {
+			for key, val := range repoConfig {
+				ret[key] = val
+			}
+		}
+	}
+
+	if ret == nil {
+		ret = make(map[string]string)
+	}
+
+	return ret
+
+}
+
+// Read a specific .gitconfig-formatted config file
+// Returns a map of setting=value, where group levels are indicated by dot-notation
+// e.g. git-lob.logfile=blah
+// all keys are converted to lower case for easier matching
+func ReadConfigFile(filepath string) (map[string]string, error) {
+	f, err := os.OpenFile(filepath, os.O_RDONLY, 0666)
+	if err != nil {
+		return make(map[string]string), err
+	}
+	defer f.Close()
+
+	return ReadConfigStream(f)
+
+}
+func ReadConfigStream(in io.Reader) (map[string]string, error) {
+	ret := make(map[string]string, 10)
+	includes := make([]string, 0, 3)
+	includeRegex := regexp.MustCompile(`^\%include\s+(.*)$"`)
+	sectionRegex := regexp.MustCompile(`^\[(.*)\]$`)                    // simple section regex ie [section]
+	namedSectionRegex := regexp.MustCompile(`^\[(.*)\s+\"(.*)\"\s*\]$`) // named section regex ie [section "name"]
+
+	scanner := bufio.NewScanner(in)
+	var currentSection string
+	var currentSectionName string
+	for scanner.Scan() {
+		// Reads lines by default, \n is already stripped
+		line := strings.TrimSpace(scanner.Text())
+		// Detect comments - discard any of the line after the comment but keep anything before
+		commentPos := strings.IndexAny(line, "#;")
+		if commentPos != -1 {
+			// skip comments
+			if commentPos == 0 {
+				continue
+			} else {
+				// just strip rest of line after the comment
+				line = strings.TrimSpace(line[0:commentPos])
+				if len(line) == 0 {
+					continue
+				}
+			}
+		}
+
+		// Look for includes
+		if match := includeRegex.FindStringSubmatch(line); match != nil {
+			includes = append(includes, match[1])
+			continue
+		}
+
+		// Check for sections
+		if secmatch := sectionRegex.FindStringSubmatch(line); secmatch != nil {
+			// named section? [section "name"]
+			if namedsecmatch := namedSectionRegex.FindStringSubmatch(line); namedsecmatch != nil {
+				// Named section
+				currentSection = namedsecmatch[1]
+				currentSectionName = namedsecmatch[2]
+
+			} else {
+				// Normal section
+				currentSection = secmatch[1]
+				currentSectionName = ""
+			}
+			continue
+		}
+
+		// Otherwise, probably a standard setting
+		equalPos := strings.Index(line, "=")
+		if equalPos != -1 {
+			name := strings.TrimSpace(line[0:equalPos])
+			value := strings.TrimSpace(line[equalPos+1:])
+			if currentSection != "" {
+				if currentSectionName != "" {
+					name = fmt.Sprintf("%v.%v.%v", currentSection, currentSectionName, name)
+				} else {
+					name = fmt.Sprintf("%v.%v", currentSection, name)
+				}
+			}
+			// convert key to lower case for easier matching
+			name = strings.ToLower(name)
+			ret[name] = value
+		}
+
+	}
+	if scanner.Err() != nil {
+		// Problem (other than io.EOF)
+		// return content we read up to here anyway
+		return ret, scanner.Err()
+	}
+
+	// TODO includes
+
+	return ret, nil
+
 }
