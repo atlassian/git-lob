87b4aa1bbb9db97c133cd3356887484171bd5cec
Steve Streeting <steve@stevestreeting.com>
2015-01-08 18:21:56 +0000

    Blocking out fetch docs

---
+++ fetch.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+// Fetch command line tool
+func cmdFetch() int {
+
+	// git-lob fetch [--all] [--prune] [--force] [<remote> [<ref>...]]
+
+	// Validate custom options
+	errorList := validateCustomOptions(GlobalOptions, nil, []string{"all", "recheck", "force"})
+	if len(errorList) > 0 {
+		fmt.Fprintf(os.Stderr, strings.Join(errorList, "\n"))
+		return 9
+	}
+
+	// TODO
+
+	return 0
+}
+
+func cmdFetchHelp() {
+	fmt.Println(`Usage: git-lob fetch [options] [<remote> [<ref>...]]
+
+  Download binaries from a remote, retrieving only the binaries referenced
+  by recent commits visible in the git repo. The definition of 'recent' depends
+  on whether you specify a list of references to fetch or not, and some
+  configuration parameters. See RECENT COMMITS below.
+
+Parameters:
+  <remote>: The remote to download from. This should correspond to the 
+            name of a remote (no direct URLs permitted) which is configured
+            in .git/config. See REMOTES below for more details, additional
+            config parameters are required in the remote.
+
+            If no remote is specified, and --all is not specified, the remote
+            that your current branch is tracking will be used, or origin if 
+            tracking is not configured.
+     <ref>: Which reference(s) we should make sure binaries downloaded for. 
+            You can specify zero, one, or many refs, but these refs must be
+            present locally (ie already fetched with plain git). git-lob
+            does not perform the 'git fetch' for you and can only look at
+            commits already present in your repo.
+
+            If no ref is specified, git-lob will look at 'recent' commits.
+            See RECENT COMMITS below.
+
+            COMMIT RANGES
+
+            You can also specify a range of refs in the form <ref1>..<ref2> to
+            tell git-lob to make sure you have all binaries referenced by all
+            commits in a given range, whether 'recent' or not.
+
+
+Options:
+  --all         Download from all configured remotes.
+  --force       Always download files even if the provider believes the file is 
+                already present locally. 
+  --prune       As well as downloading files referenced by 'recent' commits, 
+                delete any local files you already have which now fall outside
+                this definition of 'recent'. See RECENT COMMITS below.
+  --quiet, -q   Print less output
+  --verbose, -v Print more output
+  --dry-run     Don't actually delete anything, just report
+
+REMOTES
+
+Binaries in git-lob are not stored in the regular git repo, but a corresponding
+binary store must always exist to provide the actual binary content. A remote
+in git usually only gives you the real git repo, so git-lob needs to expand
+the configuration parameters to git remotes to specify the location of the 
+corresponding remote binary store. 
+
+The <remote> parameter refers to a named remote in .git/config (plain URLs 
+cannot be supported). The remote entry itself is the same as any normal git
+remote, except that it requires additional git-lob specific parameters. The
+parameters depend on the type of binary storage ('provider') being used; see
+'git-lob providers' for a list of available providers and 
+'git-lob provider <provider>' for specific details of one provider.
+
+As an example, let's take the 'filesystem' provider, which simply uses the OS's
+file system as a remote transport (obviously very simplistic):
+
+[remote "origin"]
+    # these 2 lines are standard git
+    url = ssh://git@bitbucket.org/something/somthing.git
+    fetch = +refs/heads/*:refs/remotes/origin/*
+    # these next 2 lines are required to configure the remote binary store
+    git-lob-provider = filesystem
+    git-lob-path = /Volumes/shared/something/something/binary/store
+    
+Other providers may require other parameters. It's important to note that you
+can share a binary store among multiple remote repos if you wish, much like
+the local git-lob.sharedstore option, since binaries are stored by SHA. 
+Identical file content in multiple repos can be stored only once this way.
+Of course, access control may be an issue to consider here though.
+
+RECENT COMMITS
+
+TODO
+
+`)
+}
