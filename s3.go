bbd60c1b607a1d5737d5c533f1081e4befaff20f
Steve Streeting <steve@stevestreeting.com>
2015-02-23 17:11:30 +0000

    Basic functionality for checking file existence

--- s3.go
+++ s3.go
@@ -8,11 +8,13 @@ import (
 	"os"
 	"os/user"
 	"path/filepath"
+	"strings"
 )
 
 // S3SyncProvider implements the basic SyncProvider interface for S3
 type S3SyncProvider struct {
 	S3Connection *s3.S3
+	Buckets      []string
 }
 
 func (*S3SyncProvider) TypeID() string {
@@ -27,8 +29,7 @@ func (*S3SyncProvider) HelpTextDetail() string {
 	return `The "s3" provider synchronises files with a bucket on Amazon's S3 cloud storage
 
 Required parameters in remote section of .gitconfig:
-    git-lob-s3-bucket   The bucket to use as the root remote store. Will be created if
-                        it doesn't already exist
+    git-lob-s3-bucket   The bucket to use as the root remote store. Must already exist.
     git-lob-s3-region   The AWS region to use. If not specified will use region settings
                         from your ~/.aws/config. If no region is specified, uses US East.
 
@@ -47,6 +48,9 @@ Global AWS settings:
   2. Credentials file in ~/.aws/credentials or %USERPROFILE%\.aws\credentials
 
   In addition, region settings are read from your config file in ~/.aws/config.
+  See:
+  http://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html
+  for more details on the configuration process.
 `
 }
 
@@ -106,14 +110,62 @@ func (self *S3SyncProvider) initS3() error {
 		return err
 	}
 	self.S3Connection = s3.New(auth, region)
+
+	// Read bucket list right now since we have no way to probe whether a bucket exists
+	self.S3Connection.ListBuckets()
+
+	return nil
+}
+func (self *S3SyncProvider) getS3Connection() (*s3.S3, error) {
+	if self.S3Connection == nil {
+		err := self.initS3()
+		if err != nil {
+			return nil, err
+		}
+	}
+	return self.S3Connection, nil
+}
+func (self *S3SyncProvider) getBucketName(remoteName string) (string, error) {
+	bucketsetting := fmt.Sprintf("remote.%v.git-lob-s3-bucket", remoteName)
+	bucket := strings.TrimSpace(GlobalOptions.GitConfig[bucketsetting])
+	if bucket == "" {
+		return "", fmt.Errorf("Configuration invalid for 'filesystem', missing setting %v", bucketsetting)
+	}
+	return bucket, nil
+}
+func (self *S3SyncProvider) getBucket(remoteName string) (*s3.Bucket, error) {
+	bucketname, err := self.getBucketName(remoteName)
+	if err != nil {
+		return nil, err
+	}
+	conn, err := self.getS3Connection()
+	if err != nil {
+		return nil, err
+	}
+	return conn.Bucket(bucketname), nil
+}
+
+func (self *S3SyncProvider) ValidateConfig(remoteName string) error {
+	_, err := self.getBucketName(remoteName)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
-func (*S3SyncProvider) ValidateConfig(remoteName string) error {
-	bucketsetting := fmt.Sprintf("remote.%v.git-lob-s3-bucket", remoteName)
-	bucket := GlobalOptions.GitConfig[bucketsetting]
-	if bucket == "" {
-		return fmt.Errorf("Configuration invalid for 'filesystem', missing setting %v", bucketsetting)
+func (self *S3SyncProvider) FileExists(remoteName, filename string) bool {
+	bucket, err := self.getBucket(remoteName)
+	if err != nil {
+		return false
 	}
-	return nil
+	key, err := bucket.GetKey(filename)
+	return err == nil && key != nil
+}
+func (self *S3SyncProvider) FileExistsAndIsOfSize(remoteName, filename string, sz int64) bool {
+	bucket, err := self.getBucket(remoteName)
+	if err != nil {
+		return false
+	}
+	key, err := bucket.GetKey(filename)
+	return err == nil && key != nil && key.Size == sz
 }
