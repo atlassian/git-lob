6b57816666714e39f401ef5bc1eb0862499788d3
Steve Streeting <steve@stevestreeting.com>
2015-03-13 13:06:13 +0000

    Tests for finding pushed ancestors & commits to push with new push state tracking

--- remote_test.go
+++ remote_test.go
@@ -3,12 +3,14 @@ package main
 import (
 	. "bitbucket.org/sinbad/git-lob/Godeps/_workspace/src/github.com/onsi/ginkgo"
 	. "bitbucket.org/sinbad/git-lob/Godeps/_workspace/src/github.com/onsi/gomega"
+	"fmt"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 var _ = Describe("Remote", func() {
-	Describe("Storage of remote state cache", func() {
+	Context("Simple storage of remote state cache", func() {
 
 		root := filepath.Join(os.TempDir(), "RemoteStateTest")
 		remote1Name := "origin"
@@ -65,60 +67,176 @@ var _ = Describe("Remote", func() {
 			pushed = GetPushedCommits(remote1Name)
 			Expect(pushed).To(Equal([]string{}), "Pushed should be empty after reset")
 		})
-
-		/*
-			It("finds pushed ancestor", func() {
-				// Create a bunch of empty commits, doesn't matter so long as message is different each time
-				// so commit SHA is unique
-				// Just use random SHAs to generate different messages
-				// We need > 100 to test 2 batches of commit probes
-				msgs := GetListOfRandomSHAsForTest(100)
-				var commitPoints []string
-				for i, msg := range msgs {
-					cmd := exec.Command("git", "commit", "--allow-empty", "-m", msg)
-					if err := cmd.Run(); err != nil {
-						Fail(err.Error())
-					}
-
-					// Record push points at useful test intervals
-					// start & end, in middle & near boundaries
-					if i == 0 || i == 16 || i == 50 || i == 90 || i == 99 {
-						// Get HEAD
-						cmd := exec.Command("git", "rev-parse", "HEAD")
-						outp, err := cmd.Output()
-						if err != nil {
-							Fail(err.Error())
-						}
-
-						commitPoints = append(commitPoints, strings.TrimSpace(string(outp)))
-
-					}
-				}
-				remote := "origin"
-				// Get SHA of HEAD
-				cmd := exec.Command("git", "rev-parse", "HEAD")
-				outp, err := cmd.Output()
-				if err != nil {
-					Fail(err.Error())
-				}
-				headSHA := strings.TrimSpace(string(outp))
-
-				// Check that no-match case works & terminates correctly
-				sha, err := FindLatestAncestorWhereBinariesPushed(remote, headSHA)
-				Expect(err).To(BeNil())
-				Expect(sha).To(Equal(""), "Should be no pushed binaries at start")
-
-				for i, pushedCommit := range commitPoints {
-					// Say we've pushed at this point, then test from HEAD
-					SuccessfullyPushedBinariesForCommit_REMOVE(remote, pushedCommit)
-
-					sha, err := FindLatestAncestorWhereBinariesPushed(remote, headSHA)
-					Expect(err).To(BeNil())
-					Expect(sha).To(Equal(pushedCommit), "Should detect %v as pushed commit (iteration %d)", pushedCommit, i)
-				}
-
-			})
-		*/
-
 	})
+
+	Context("Real git repo tests", func() {
+		root := filepath.Join(os.TempDir(), "RemoteStateTest")
+		var oldwd string
+		var setupInputs []*TestCommitSetupInput
+		var setupOutputs []*CommitLOBRef
+		var getOutputSubset = func(indexes ...int) []*CommitLOBRef {
+			ret := make([]*CommitLOBRef, 0, len(indexes))
+			for _, i := range indexes {
+				ret = append(ret, setupOutputs[i])
+			}
+			return ret
+		}
+		BeforeEach(func() {
+			CreateGitRepoForTest(root)
+			oldwd, _ = os.Getwd()
+			os.Chdir(root)
+
+			now := time.Now()
+			// Create a relatively interesting git repo with a few branches, merges
+			setupInputs = []*TestCommitSetupInput{
+				&TestCommitSetupInput{ // 0
+					CommitDate: now.AddDate(0, 0, -29),
+					Files:      []string{"data1.bin", filepath.Join("img", "image1.jpg")},
+				},
+				&TestCommitSetupInput{ // 1
+					CommitDate: now.AddDate(0, 0, -28),
+					Files:      []string{"data2.bin", filepath.Join("img", "image2.jpg")},
+				},
+				// branch, modify & add
+				// this branch we'll leave hanging
+				&TestCommitSetupInput{ // 2
+					CommitDate: now.AddDate(0, 0, -27),
+					Files:      []string{"data1.bin", filepath.Join("bigdata", "something.dat")},
+					NewBranch:  "feature/hanging",
+				},
+				&TestCommitSetupInput{ // 3
+					CommitDate:     now.AddDate(0, 0, -23),
+					Files:          []string{"data3.bin", "data4.bin", "data5.bin"},
+					ParentBranches: []string{"feature/hanging"},
+				},
+				// now back on master
+				&TestCommitSetupInput{ // 4
+					CommitDate:     now.AddDate(0, 0, -25),
+					Files:          []string{"data2.bin", "newfile1.dat", "newfile2.dat"},
+					ParentBranches: []string{"master"},
+				},
+				&TestCommitSetupInput{ // 5
+					CommitDate: now.AddDate(0, 0, -24),
+					Files:      []string{"mergedata.bin", "mergedata2.dat"},
+					NewBranch:  "feature/tomerge",
+				},
+				// add a few parallel commits on master
+				&TestCommitSetupInput{ // 6
+					CommitDate:     now.AddDate(0, 0, -24),
+					Files:          []string{"parallel.dat"},
+					ParentBranches: []string{"master"}, // unnecessary but make sure
+				},
+				&TestCommitSetupInput{ // 7
+					CommitDate:     now.AddDate(0, 0, -23),
+					Files:          []string{"mergedata.bin", "mergedata3.dat"},
+					ParentBranches: []string{"feature/tomerge"},
+				},
+				// add a few parallel commits on master
+				&TestCommitSetupInput{ // 8
+					CommitDate:     now.AddDate(0, 0, -23),
+					Files:          []string{"parallel2.dat"},
+					ParentBranches: []string{"master"}, // unnecessary but make sure
+				},
+				// now merge (no changes added except from merge)
+				&TestCommitSetupInput{ // 9
+					CommitDate:     now.AddDate(0, 0, -22),
+					ParentBranches: []string{"master", "feature/tomerge"},
+				},
+				// one more commit on master
+				&TestCommitSetupInput{ // 10
+					CommitDate:     now.AddDate(0, 0, -20),
+					Files:          []string{filepath.Join("img", "image30.jpg")},
+					ParentBranches: []string{"master"}, // unnecessary but make sure
+				},
+				// commit with no files
+				&TestCommitSetupInput{ // 11
+					CommitDate:     now.AddDate(0, 0, -20),
+					ParentBranches: []string{"master"}, // unnecessary but make sure
+				},
+				// now one more hanging branch ahead of  master
+				&TestCommitSetupInput{ // 12
+					CommitDate:     now.AddDate(0, 0, -10),
+					Files:          []string{filepath.Join("img", "image31.jpg")},
+					ParentBranches: []string{"master"}, // unnecessary but make sure
+					NewBranch:      "feature/hanging_ahead",
+				},
+				// one more commit hanging
+				&TestCommitSetupInput{ // 13
+					CommitDate:     now.AddDate(0, 0, -9),
+					Files:          []string{filepath.Join("img", "image40.jpg")},
+					ParentBranches: []string{"feature/hanging_ahead"}, // unnecessary but make sure
+				},
+			}
+
+			setupOutputs = SetupRepoForTest(setupInputs)
+
+		})
+		AfterEach(func() {
+			os.Chdir(oldwd)
+			os.RemoveAll(root)
+		})
+
+		It("traces pushed ancestors", func() {
+			remote := "origin"
+
+			// Check that no-match case works & terminates correctly
+			sha, err := FindLatestAncestorWhereBinariesPushed(remote, "master")
+			Expect(err).To(BeNil())
+			Expect(sha).To(Equal(""), "Should be no pushed binaries at start")
+
+			// Mark pushed at feature/hanging but not at tip
+			err = MarkBinariesAsPushed(remote, setupOutputs[2].commit, "")
+			Expect(err).To(BeNil())
+			// The ancestor that's pushed should be the common parent of hanging branch & master,
+			// which is index 1
+			sha, err = FindLatestAncestorWhereBinariesPushed(remote, "master")
+			Expect(err).To(BeNil())
+			Expect(sha).To(Equal(setupOutputs[1].commit), "Should find common ancestor that's pushed")
+
+			// Mark a commit on master as pushed too
+			err = MarkBinariesAsPushed(remote, setupOutputs[4].commit, "")
+			Expect(err).To(BeNil())
+			// The ancestor that's pushed from master should now be this one
+			sha, err = FindLatestAncestorWhereBinariesPushed(remote, "master")
+			Expect(err).To(BeNil())
+			Expect(sha).To(Equal(setupOutputs[4].commit), "Should find latest ancestor that's pushed")
+			// but if measured from hanging branch this should be its own
+			sha, err = FindLatestAncestorWhereBinariesPushed(remote, "feature/hanging")
+			Expect(err).To(BeNil())
+			Expect(sha).To(Equal(setupOutputs[2].commit), "Should find hanging branch ancestor that's pushed")
+
+			// Now mark a commit on the merge line as pushed, but not where it's merged
+			// Again don't mark the tip to make the test more interesting
+			err = MarkBinariesAsPushed(remote, setupOutputs[5].commit, "")
+			Expect(err).To(BeNil())
+			// The ancestor that's pushed from master should now be the one in the middle of the merge
+			sha, err = FindLatestAncestorWhereBinariesPushed(remote, "master")
+			Expect(err).To(BeNil())
+			Expect(sha).To(Equal(setupOutputs[5].commit), "Should find merge ancestor that's pushed")
+			// but should not affect hanging branch this should be its own
+			sha, err = FindLatestAncestorWhereBinariesPushed(remote, "feature/hanging")
+			Expect(err).To(BeNil())
+			Expect(sha).To(Equal(setupOutputs[2].commit), "Should find hanging branch ancestor that's pushed")
+			// However even though merge ancestor is 'latest pushed' from master, we should still see commits on master in parallel with merge to push
+			commits, err := GetCommitLOBsToPushForRef(remote, "master", false)
+			Expect(err).To(BeNil())
+			// don't include commits with no files e.g. 9(merge) & 11
+			Expect(commits).To(ConsistOf(getOutputSubset(6, 7, 8, 10)), "Should be correct list to push")
+			// now mark something in the middle on master as pushed & make sure remnants of merge branch still get picked up
+			// Also replace the previously marked SHA on master because assume we picked this up from master (since merged) but didn't finish
+			err = MarkBinariesAsPushed(remote, setupOutputs[8].commit, setupOutputs[4].commit)
+			Expect(err).To(BeNil())
+			// The ancestor that's pushed from master should now be this one
+			sha, err = FindLatestAncestorWhereBinariesPushed(remote, "master")
+			Expect(err).To(BeNil())
+			Expect(sha).To(Equal(setupOutputs[8].commit), "Should find master ancestor that's pushed")
+			// Now test we pick up the commits from the merge branch on master, after what was marked there
+			commits, err = GetCommitLOBsToPushForRef(remote, "master", false)
+			Expect(err).To(BeNil())
+			// don't include commits with no files e.g. 9(merge) & 11
+			Expect(commits).To(ConsistOf(getOutputSubset(7, 10)), "Should be correct list to push")
+
+		})
+	})
+
 })
