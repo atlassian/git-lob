c9ba8fad31a56b9e79ade7533d0444af14f4e53f
Steve Streeting <steve@stevestreeting.com>
2015-03-31 13:03:55 +0100

    Remove build versioning via -ldflags, doesn't work outside trivial main package case

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Install script which inserts a build ID into the code (short git hash)
-# requires that git & go are on the path, which of course they are in your environment
-
-sha=`git rev-parse --short HEAD`
-go install -ldflags "-X main.VersionBuildID $sha"
--- util/globals.go
+++ util/globals.go
@@ -5,17 +5,12 @@ import (
 )
 
 var (
-	GlobalOptions  *Options = NewOptions()
-	VersionMajor            = 0
-	VersionMinor            = 4
-	VersionPatch            = 0
-	VersionBuildID string   // populated in build.sh to the git hash
+	GlobalOptions *Options = NewOptions()
+	VersionMajor           = 0
+	VersionMinor           = 5
+	VersionPatch           = 0
 )
 
 func Version() string {
-	if VersionBuildID != "" {
-		return fmt.Sprintf("%d.%d.%d [%v]", VersionMajor, VersionMinor, VersionPatch, VersionBuildID)
-	} else {
-		return fmt.Sprintf("%d.%d.%d", VersionMajor, VersionMinor, VersionPatch)
-	}
+	return fmt.Sprintf("%d.%d.%d", VersionMajor, VersionMinor, VersionPatch)
 }
